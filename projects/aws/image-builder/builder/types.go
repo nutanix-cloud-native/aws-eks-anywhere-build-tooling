5a41cf50fe1648db3de90fb7aa228e322688d2c7
deepakm-ntnx <deepak.muley@nutanix.com>
2022-09-16 12:25:30 -0700

    initial checkin for build nutanix image thru image-builder

--- projects/aws/image-builder/builder/types.go
+++ projects/aws/image-builder/builder/types.go
@@ -4,12 +4,14 @@ const (
 	Ubuntu    string = "ubuntu"
 	VSphere   string = "vsphere"
 	Baremetal string = "baremetal"
+	NutanixAHV       string = "nutanixahv"
 )
 
 type BuildOptions struct {
 	Os              string
 	Hypervisor      string
 	VsphereConfig   string
+	NutanixAHVConfig   string
 	ReleaseChannel  string
 	artifactsBucket string
 	Force           bool
